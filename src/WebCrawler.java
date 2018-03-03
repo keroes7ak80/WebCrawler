0e74e4e91ad1edcb6e3aad48c6b3369adfb517bd
keroes7ak2 <kerollos.takla95@eng-st.cu.edu.eg>
2018-03-03 17:11:17 +0200

    aa

--- src/WebCrawler.java
+++ src/WebCrawler.java
@@ -8,15 +8,7 @@ public class WebCrawler {
 
     public static void main(String args[]) {
 
-
         try {
-//            url = new URL("https://stackoverflow.com/");
-//            is = url.openStream();  // throws an IOException
-//            br = new BufferedReader(new InputStreamReader(is));
-//
-//            while ((line = br.readLine()) != null) {
-//                System.out.println(line);
-//            }
             Document doc=Jsoup.connect("https://jsoup.org/bugs").get();
             org.jsoup.select.Elements links =doc.select("a");
             for(Element e :links) {
